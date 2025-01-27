6f9bb9f3d0456343cbb7de39e9c6e4a1626892c8
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2025-01-27 21:19:53 +0200

    add two tasks

---
+++ isUnique.js
@@ -0,0 +1,5 @@
+function isUnique(nums) {
+    return  new Set([...nums]).size === nums.length;
+}
+
+console.log('result', isUnique([1, 2 ,3 , 4,]))
---
+++ maxNumber.js
@@ -0,0 +1,5 @@
+const numbers = [3, 5, 7, 2, 8];
+
+const result = Math.max(...numbers);
+
+console.log('result', result)
