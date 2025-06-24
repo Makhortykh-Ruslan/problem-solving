a1620c4970b6a0d679f8346672182fdeefff1bb4
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2025-06-24 23:57:57 +0300

    add minSubArrayLen

---
+++ problem-solving/sliding-window/minSubArrayLen.js
@@ -0,0 +1,25 @@
+export const minSubArrayLen = (target, nums) => {
+    let left = 0;
+    let minLength = Infinity;
+    let currentSumWindow = 0;
+    let windowSize = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        currentSumWindow += nums[right];
+
+        while (currentSumWindow >= target) {
+            windowSize = right - left + 1
+            minLength = Math.min(minLength, windowSize);
+            currentSumWindow -= nums[left];
+            left++;
+        }
+
+    }
+
+    return minLength === Infinity ? 0 : minLength;
+
+}
+
+const result = minSubArrayLen(7, [2,3,1,2,4,3]);
+
+console.log('result', result)
