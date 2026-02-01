dfaf07c18dae69082a90be893fb048bb72298f6e
chi.dev <makhortykh.ruslan@gmail.com>
2026-02-01 17:32:30 +0200

    getErrorCorrectly

--- other/tryCatchProblem.js
+++ other/tryCatchProblem.js
@@ -15,14 +15,49 @@
 
 // uploadData()
 
-async function fetchWithRetry(url) {
-    try {
-        return await fetch(url);
-    } catch (error) {
-        console.log("First try error...");
+// async function fetchWithRetry(url) {
+//     try {
+//         return await fetch(url);
+//     } catch (error) {
+//         console.log("First try error...");
 
-        await new Promise(resolve => setTimeout(() => resolve(), 1000));
+//         await new Promise(resolve => setTimeout(() => resolve(), 1000));
 
-        return await fetch(url);
-    }
+//         return await fetch(url);
+//     }
+// }
+
+process.on('uncaughtException', (err) => {
+    console.log('NodeJS перехопив критичний throw:', err.message);
+});
+
+async function getErrorCorrectly() {
+    await new Promise(res => setTimeout(res, 2000)); 
+    throw new Error('getError');
 }
+
+function getDataProducts() {
+    return new Promise((resolve, reject) => setTimeout(() => resolve('getDataProducts'), 1000))
+}
+
+function getDataCards() {
+    return new Promise((resolve) => setTimeout(() => resolve('getDataCards'), 1000))
+}
+
+function getDataMembers() {
+    return new Promise((resolve) => setTimeout(() => resolve('getDataMembers'), 1000))
+}
+
+try {
+    const initData = await Promise.all([
+        getDataProducts(),
+        getDataCards(),
+        getDataMembers(),
+        getErrorCorrectly()
+    ])
+
+    console.log(initData)
+} catch (error) {
+    console.log('Error:', error)
+}
+
