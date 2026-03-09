a86d5f03d1edea298df5217c51cc9179ee1dd2a4
chi.dev <makhortykh.ruslan@gmail.com>
2026-03-09 21:51:19 +0200

    add: isAlphanumeric

--- patterns/two-pointers/valid-palindrome.js
+++ patterns/two-pointers/valid-palindrome.js
@@ -21,6 +21,16 @@
  * @param {string} s
  * @return {boolean}
  */
+
+const isAlphanumeric = (char) => {
+  const code = char.charCodeAt(0);
+  return (
+    (code >= 48 && code <= 57) || // 0-9
+    (code >= 65 && code <= 90) || // A-Z
+    (code >= 97 && code <= 122)
+  ); // a-z
+};
+
 const isPalindrome = function (s) {
   const isAlphanumeric = (char) => /[a-zA-Z0-9]/.test(char);
 
