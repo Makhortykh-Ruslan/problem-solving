2ed55e2528107f304c1875e9b542b81a6bba919b
chi.dev <makhortykh.ruslan@gmail.com>
2026-03-18 21:19:36 +0200

    TASK: lengthOfLongestSubstring

---
+++ patterns/sliding-window/lengthOfLongestSubstring.js
@@ -0,0 +1,30 @@
+/**
+ * Problem: Longest Substring Without Repeating Characters
+ * Pattern: Sliding Window (Dynamic)
+ * Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ *
+ * Description:
+ * Given a string s, find the length of the longest substring
+ * without repeating characters.
+ *
+ * Example:
+ * Input: s = "abcabcbb"
+ * Output: 3 ("abc")
+ *
+ * Input: s = "bbbbb"
+ * Output: 1 ("b")
+ *
+ * Input: s = "pwwkew"
+ * Output: 3 ("wke")
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = function (s) {};
+
+// Test cases
+console.log(lengthOfLongestSubstring('abcabcbb')); // Expected: 3
+console.log(lengthOfLongestSubstring('bbbbb')); // Expected: 1
+console.log(lengthOfLongestSubstring('pwwkew')); // Expected: 3
