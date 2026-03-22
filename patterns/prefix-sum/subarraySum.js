c1746f584aceaad4a746495cfb2effdb016942e5
chi.dev <makhortykh.ruslan@gmail.com>
2026-03-22 20:09:43 +0200

    TASK: subarraySum

---
+++ patterns/prefix-sum/subarraySum.js
@@ -0,0 +1,36 @@
+/**
+ * Problem: Subarray Sum Equals K
+ * Pattern: Prefix Sum + HashMap
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/subarray-sum-equals-k/
+ *
+ * Description:
+ * Given an array of integers nums and an integer k,
+ * return the total number of subarrays whose sum equals to k.
+ * A subarray is a contiguous non-empty sequence of elements within an array.
+ *
+ * Example:
+ * Input: nums = [1,1,1], k = 2
+ * Output: 2
+ *
+ * Input: nums = [1,2,3], k = 3
+ * Output: 2
+ *
+ * Constraints:
+ * - 1 <= nums.length <= 2 * 10^4
+ * - -1000 <= nums[i] <= 1000
+ * - -10^7 <= k <= 10^7
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const subarraySum = function (nums, k) {
+  for (let i = 0; i < nums.length; i++) {}
+};
+
+// Test cases
+console.log(subarraySum([1, 1, 1], 2)); // Expected: 2
+console.log(subarraySum([1, 2, 3], 3)); // Expected: 2
