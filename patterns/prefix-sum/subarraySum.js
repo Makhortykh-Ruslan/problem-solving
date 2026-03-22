2b3f64766fcbc189d78adcac5125fbfc636bdd34
chi.dev <makhortykh.ruslan@gmail.com>
2026-03-22 21:16:56 +0200

    SOLV: subarraySum

--- patterns/prefix-sum/subarraySum.js
+++ patterns/prefix-sum/subarraySum.js
@@ -28,7 +28,25 @@
  * @return {number}
  */
 const subarraySum = function (nums, k) {
-  for (let i = 0; i < nums.length; i++) {}
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let sum = nums[i];
+
+    if (sum === k) {
+      count++;
+    }
+
+    for (let j = i + 1; j < nums.length; j++) {
+      sum += nums[j];
+
+      if (sum === k) {
+        count++;
+      }
+    }
+  }
+
+  return count;
 };
 
 // Test cases
