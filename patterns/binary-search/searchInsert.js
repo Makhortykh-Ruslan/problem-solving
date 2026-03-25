0ab40cef5aaf265fd3c1d22c243499d9750a6f8c
chi.dev <makhortykh.ruslan@gmail.com>
2026-03-25 16:51:27 +0200

    SOLV: searchInsert

--- patterns/binary-search/searchInsert.js
+++ patterns/binary-search/searchInsert.js
@@ -33,10 +33,29 @@
  * @return {number}
  */
 const searchInsert = function (nums, target) {
-  // your code here
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      left = mid;
+      break;
+    } else if (target >= nums[mid]) {
+      left = mid + 1;
+    } else if (target <= nums[mid]) {
+      right = mid - 1;
+    }
+  }
+
+  return left;
 };
 
 // Test cases
+console.log(searchInsert([1, 3], 2)); // Expected: 1
+console.log(searchInsert([1], 1)); // Expected: 0
+console.log(searchInsert([1, 3, 5, 6], 0)); // Expected: 0
 console.log(searchInsert([1, 3, 5, 6], 5)); // Expected: 2
 console.log(searchInsert([1, 3, 5, 6], 2)); // Expected: 1
 console.log(searchInsert([1, 3, 5, 6], 7)); // Expected: 4
