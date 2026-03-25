34c93fab4bc8ac8e80130e9edbc584f07e193af4
chi.dev <makhortykh.ruslan@gmail.com>
2026-03-25 17:34:43 +0200

    TASK: isBadVersion

---
+++ patterns/binary-search/isBadVersion.js
@@ -0,0 +1,43 @@
+/**
+ * Problem: First Bad Version
+ * Pattern: Binary Search
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/first-bad-version/
+ *
+ * Description:
+ * You are a product manager and currently leading a team to develop a new product.
+ * Unfortunately, the latest version of your product fails the quality check.
+ * Since each version is developed based on the previous version, all the versions
+ * after a bad version are also bad.
+ *
+ * You are given an API isBadVersion(version) which returns whether version is bad.
+ * Implement a function to find the first bad version.
+ * You should minimize the number of calls to the API.
+ *
+ * Example:
+ * Input: n = 5, bad = 4
+ * Output: 4
+ *
+ * Input: n = 1, bad = 1
+ * Output: 1
+ *
+ * Constraints:
+ * - 1 <= bad <= n <= 2^31 - 1
+ */
+
+const isBadVersion = (bad) => (version) => version >= bad;
+const isBad = isBadVersion(4);
+
+/**
+ * @param {function} isBadVersion
+ * @return {function}
+ */
+const solution = function (isBadVersion) {
+  return function (n) {
+    // your code here
+  };
+};
+
+// Test cases
+console.log(solution(isBad)(5)); // Expected: 4
+console.log(solution(isBadVersion(1))(1)); // Expected: 1
