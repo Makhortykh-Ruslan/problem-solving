e184dc98f4185ab501a351f27b42bf98dbb13e64
chi.dev <makhortykh.ruslan@gmail.com>
2026-04-09 21:29:51 +0300

    SOLV: firstUniqChar

--- playground/firstUniqChar.js
+++ playground/firstUniqChar.js
@@ -1,19 +1,57 @@
-function firstUniqChar(s) {
-  const hashMap = new Map();
+/**
+ * Problem: First Unique Character in a String
+ * Pattern: HashMap
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/first-unique-character-in-a-string/
+ *
+ * Description:
+ * Given a string s, find the first non-repeating character
+ * and return its index. If it does not exist, return -1.
+ *
+ * Example:
+ * Input: s = "leetcode"
+ * Output: 0
+ *
+ * Input: s = "loveleetcode"
+ * Output: 2
+ *
+ * Input: s = "aabb"
+ * Output: -1
+ *
+ * Constraints:
+ * - 1 <= s.length <= 10^5
+ * - s consists of only lowercase English letters
+ */
 
-  for (let i = 0; i < s.length; i++) {
-    const char = s[i];
-    hashMap.set(char, (hashMap.get(char) || 0) + 1);
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const firstUniqChar = function (s) {
+  const hashMap = new Map();
+  let step = 0;
+
+  while (step < s.length) {
+    hashMap.set(s[step], (hashMap.get(s[step]) || 0) + 1);
+    step++;
   }
 
-  for (let i = 0; i < s.length; i++) {
-    const char = s[i];
-    if (hashMap.get(char) === 1) {
-      return i;
+  step = 0;
+
+  while (step < s.length) {
+    const result = hashMap.get(s[step]);
+
+    if (result === 1) {
+      return step;
     }
+
+    step++;
   }
 
   return -1;
-}
+};
 
-module.exports = { firstUniqChar };
+// Test cases
+console.log(firstUniqChar('leetcode')); // 0
+console.log(firstUniqChar('loveleetcode')); // 2
+console.log(firstUniqChar('aabb')); // -1
