963240b8f5366ba4a60306c28aa30e0752637c5d
chi.dev <makhortykh.ruslan@gmail.com>
2026-04-21 22:26:36 +0300

    SOLV: merge

--- patterns/two-pointers/merge.js
+++ patterns/two-pointers/merge.js
@@ -34,9 +34,44 @@
  * @param {number} n
  * @return {void}
  */
-const merge = function (nums1, m, nums2, n) {};
+// const merge = function (nums1, m, nums2, n) {
+//   let p1 = m - 1;
+//   let p2 = n - 1;
+//   let lastP = m + n - 1;
+//
+//   while (p2 >= 0) {
+//     if (p1 < 0) {
+//       nums1[lastP] = nums2[p2];
+//       p2--;
+//       lastP--;
+//     } else if (nums1[p1] < nums2[p2]) {
+//       nums1[lastP] = nums2[p2];
+//       p2--;
+//       lastP--;
+//     } else if (nums1[p1] >= nums2[p2]) {
+//       nums1[lastP] = nums1[p1];
+//       p1--;
+//       lastP--;
+//     }
+//   }
+//
+//   return nums1;
+// };
+
+const merge = function (nums1, m, nums2, n) {
+  let right = m;
+
+  for (let i = 0; i < nums2.length; i++) {
+    nums1[right] = nums2[i];
+    right++;
+  }
+
+  nums1.sort((a, b) => a - b);
+
+  return nums1;
+};
 
 // Test cases
-merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3); // [1,2,2,3,5,6]
-merge([1], 1, [], 0); // [1]
-merge([0], 0, [1], 1); // [1]
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); // [1,2,2,3,5,6]
+console.log(merge([1], 1, [], 0)); // [1]
+console.log(merge([0], 0, [1], 1)); // [1]
